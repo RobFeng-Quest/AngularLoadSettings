c9705ddde362e06ae6b64329760e6c54ca84cf3c
Rob Feng <rob.feng@quest.com>
2018-04-09 00:27:08 +0800

    copy a example from https://github.com/mauriciovigolo/keycloak-angular/tree/master/examples/keycloak-heroes

---
+++ src/app/utils/app-init.ts
@@ -0,0 +1,25 @@
+import { KeycloakService } from 'keycloak-angular';
+
+export function initializer(keycloak: KeycloakService): () => Promise<any> {
+  return (): Promise<any> => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await keycloak.init({
+          config: {
+            url: 'https://id-qa.quest.com/auth', // .ie: http://localhost:8080/auth/
+            realm: 'quest', // .ie: master
+            clientId: "your client id" // .ie: account
+          },
+          initOptions: {
+            onLoad: 'login-required',
+            checkLoginIframe: false
+          },
+          bearerExcludedUrls: []
+        });
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  assets: {
+    dotaImages: 'https://api.opendota.com/apps/dota2/images'
+  },
+  apis: {
+    dota: 'https://api.opendota.com/api'
+  }
+};
